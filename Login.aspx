66586ab6810038531e9907892d4b5f99163ecbeb
Zachary Lang <langzg@dukes.jmu.edu>
2020-10-14 21:34:33 -0400

    Seems to be working label

--- Login.aspx
+++ Login.aspx
@@ -10,6 +10,7 @@
     <form id="form1" runat="server">
         <div>
             <asp:Label ID="lblLogin" runat="server" Text="Welcome to Cyber City!"></asp:Label>
+            <asp:Label ID="Label1" runat="server" Text="Seems to be working"></asp:Label>
         </div>
     </form>
 </body>
--- Login.aspx.designer.cs
+++ Login.aspx.designer.cs
@@ -31,5 +31,14 @@ namespace CyberCity
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblLogin;
+
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }

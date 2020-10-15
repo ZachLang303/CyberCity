d24d6b9564f89114485feadedec689e98c48ac0a
Brian Mantone <mantonbt@dukes.jmu.edu>
2020-10-14 22:49:56 -0400

    balls

--- Login.aspx
+++ Login.aspx
@@ -11,6 +11,8 @@
         <div>
             <asp:Label ID="lblLogin" runat="server" Text="Welcome to Cyber City!"></asp:Label>
             <asp:Label ID="Label1" runat="server" Text="Seems to be working"></asp:Label>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="I miss my balls"></asp:Label>
         </div>
     </form>
 </body>
--- Login.aspx.designer.cs
+++ Login.aspx.designer.cs
@@ -40,5 +40,14 @@ namespace CyberCity
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Label2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
     }
 }
